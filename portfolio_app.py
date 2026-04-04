4394974a5a44895e4b93b9101426bdd1cd906243
benstrikes <75196091+benstrikes@users.noreply.github.com>
2026-04-04 15:40:59 +0400

    Update portfolio_app.py

--- portfolio_app.py
+++ portfolio_app.py
@@ -518,4 +518,4 @@ def index():
 
 if __name__ == "__main__":
     print("🚀  Portfolio running → http://localhost:8080")
-    app.run(debug=True, port=8080)
+    app.run(debug=True, port=8080, host='0.0.0.0')
